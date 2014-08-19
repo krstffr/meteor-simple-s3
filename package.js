d31052749292421c5639d097bdcb657e369814d0
Kristoffer Klintberg <krstffr@gmail.com>
2014-08-19 17:04:45 +0200

    Some styling etc.

--- package.js
+++ package.js
@@ -6,15 +6,16 @@ Package.on_use(function (api) {
 
 	api.use('templating', 'client');
   api.use('aws-sdk', 'server');
-  api.use(['underscore'], ['client', 'server']);
+  api.use('underscore', ['client', 'server']);
 
   api.add_files('lib/simple-s3-client.js', ['client']);
-  api.add_files('lib/simple-s3-router.js', ['client', 'server']);
-
   api.add_files('lib/simple-s3-server.js', ['server']);
 
   // Views
   api.add_files(['lib/views/simple-s3-view-home.html', 'lib/views/simple-s3-view-home.js'], 'client');
+
+  // CSS
+  api.add_files(['lib/css/stylesheets/simple-s3-styles.css'], 'client');
 
   if (typeof api.export !== 'undefined') {
 

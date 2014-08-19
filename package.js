de140530b6314404af6aba89bbfd179aa8f48425
Kristoffer Klintberg <krstffr@gmail.com>
2014-08-19 15:04:35 +0200

    Git INIT!

---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  "summary": "UI for viewing, uploading and deleting files from your AWS S3."
+});
+
+Package.on_use(function (api) {
+
+	api.use('templating', 'client');
+  api.use('aws-sdk', 'server');
+  api.use(['underscore'], ['client', 'server']);
+
+  api.add_files('lib/simple-s3-client.js', ['client']);
+  api.add_files('lib/simple-s3-router.js', ['client', 'server']);
+
+  api.add_files('lib/simple-s3-server.js', ['server']);
+
+  // Views
+  api.add_files(['lib/views/simple-s3-view-home.html', 'lib/views/simple-s3-view-home.js'], 'client');
+
+  if (typeof api.export !== 'undefined') {
+
+    // The main object.
+    api.export('SimpleS3', 'client');
+
+  }
+
+});

a35b82d6bd8d045936751ac86e69bc55111585d9
Kristoffer Klintberg <krstffr@gmail.com>
2014-10-30 10:16:45 +0100

    Removed dependecny on Meteor packages, now straight to Npm aws-sdk.

--- package.js
+++ package.js
@@ -2,27 +2,34 @@ Package.describe({
   name: "krstffr:simple-s3",
   summary: "UI for viewing, uploading and deleting files from your AWS S3.",
   git: "https://github.com/krstffr/meteor-simple-s3",
-  version: "0.0.3"
+  version: "0.0.4"
+});
+
+Npm.depends({
+  'aws-sdk': '2.0.23'
 });
 
 Package.onUse(function (api) {
 
   api.versionsFrom("METEOR@0.9.0");
 
-  api.use('peerlibrary:aws-sdk');
   api.use('templating');
 
-  api.add_files('lib/simple-s3-client.js', ['client']);
-  api.add_files('lib/simple-s3-server.js', ['server']);
+  // Server files
+  api.add_files([
+    'lib/aws-blocking-methods.js',
+    'lib/simple-s3-server.js'],
+    'server');
 
-  // Views
-  api.add_files(['lib/views/simple-s3-view-home.html', 'lib/views/simple-s3-view-home.js'], 'client');
-
-  // CSS
-  api.add_files(['lib/css/stylesheets/simple-s3-styles.css'], 'client');
+  // Client files
+  api.add_files([
+    'lib/simple-s3-client.js',
+    'lib/views/simple-s3-view-home.html',
+    'lib/views/simple-s3-view-home.js',
+    'lib/css/stylesheets/simple-s3-styles.css'],
+    'client');
 
   // The main object.
-  api.export('AWS', 'server');
   api.export('SimpleS3', 'client');
 
 });
